d1e76cdaa8db1673549c2b807eb3df2adf7589d2
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-06-22 20:17:29 -0500

    Clean up tasks.py (#19)
    Creates the missing invoke targets based on the content of the
    `Makefile`.
    
    Cleans up the invoke tasks to make them reuse the same venv for all the
    mkdocs commands.
    
    Uses `inv` and `nox` in CircleCI.

--- noxfile.py
+++ noxfile.py
@@ -10,9 +10,3 @@ def venv(session):
     """Setup the developper environment."""
     session.install("--upgrade", "pip", "setuptools")
     session.install("-r", "requirements.txt")
-
-
-@nox.session()
-def serve(session):
-    """Serve the documentation using the development server."""
-    session.run("mkdocs", "serve")
